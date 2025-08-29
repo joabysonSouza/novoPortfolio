cc181432913a263ff2fdef786e832b5bfdb0c0dc
Joabyson <joabyson916@gmail.com>
2025-08-28 23:25:06 -0300

    feat: "criando constantes para os links de navegação"

---
+++ app/constants/index.ts
@@ -0,0 +1,9 @@
+const navLinks = [
+  { label: "Trabalhos", href: "/Trabalhos", id: 1},
+  { label: "Sobre", href: "/sobre" ,id: 2},
+  { label: "Contatos", href: "/Contatos" ,id: 3},
+  { label: "Feedback", href: "/Feedback", id: 4},
+];
+
+export {navLinks}
+
