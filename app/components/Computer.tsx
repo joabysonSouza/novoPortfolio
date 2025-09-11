86e68d4c1d7910c35c76698c22bb1fc3d6bf90ff
Joabyson <joabyson916@gmail.com>
2025-09-10 22:36:22 -0300

    refactor"alterando o tamanho do modelo  3d e melhorando a visibilidade"

--- app/components/Computer.tsx
+++ app/components/Computer.tsx
@@ -3,21 +3,24 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
-
 function Computer() {
   const { scene } = useGLTF("/image/Teste.glb");
-  return <primitive object={scene} scale={1} position={[3, -2, 3]} />;
+  return (
+    <group scale={0.5} position={[0, -1, 0]} rotation={[0, Math.PI, 0]}>
+      <primitive object={scene} />
+    </group>
+  );
 }
 
 export default function Scene() {
   return (
-    <Canvas camera={{position: [1, 1, 1],}}>
+    <Canvas camera={{ position: [-8, 2, 4], fov: 50 }}>
       <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 0 , 0]} />
+      <directionalLight position={[3, 3, 3]} />
       <Suspense fallback={null}>
         <Computer />
       </Suspense>
-      <OrbitControls />
+      <OrbitControls minDistance={3} maxDistance={20} />
     </Canvas>
   );
 }
