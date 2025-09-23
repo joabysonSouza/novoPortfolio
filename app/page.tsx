f2ef603c699d6150775b9d0ab7971ada366fe19d
Joabyson <joabyson916@gmail.com>
2025-09-22 23:10:44 -0300

    feat"adicionado o componente image e  criando títulos"

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
+import Scene from "./components/Computer";
 import NavBar from "./components/NavBar";
 
-
- const Home =()=> {
+const Home = () => {
   return (
     <>
-    <div>
-      Portfolio develop 3D 
-    </div>
-    <NavBar/>
-    </>
-   
-  );
-}
+      <div className="w-full h-screen flex flex-col items-center justify-center md:flex-row ">
+        <div className=" w-full m-auto p-4 text-center ">
+          <h1 className=" font-anton  text-white text-3xl md:text-7xl">
+            Olá eu sou <span className="text-red-600">Joabyson</span>
+          </h1>
 
-export default Home
+          <p className="w-96">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            reprehenderit repudiandae
+          </p>
+        </div>
+
+        <Scene />
+      </div>
+    </>
+  );
+};
+
+export default Home;
