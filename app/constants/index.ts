edd7aa63c2c4ac6e7dabadfe67ef7f67054ec295
Joabyson <joabyson916@gmail.com>
2025-09-24 23:45:46 -0300

    feat:" criando  constante skills para melhor manuseio "

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,5 @@
+import { SiJavascript, SiNextdotjs, SiNodedotjs, SiPostgresql, SiReact, SiTailwindcss } from "react-icons/si";
+
 const navLinks = [
   { label: "Trabalhos", href: "/Trabalhos", id: 1},
   { label: "Sobre", href: "/sobre" ,id: 2},
@@ -5,5 +7,43 @@ const navLinks = [
   { label: "Feedback", href: "/Feedback", id: 4},
 ];
 
-export {navLinks}
+
+
+
+ const skills = [
+  {
+    title: "JavaScript",
+    icon: SiJavascript,
+    color: "text-yellow-400",
+  },
+  {
+    title: "React",
+    icon: SiReact,
+    color: "text-cyan-400",
+  },
+  {
+    title: "Next.js",
+    icon: SiNextdotjs,
+    color: "text-white", // geralmente branco ou preto
+  },
+  {
+    title: "Node.js",
+    icon: SiNodedotjs,
+    color: "text-green-500",
+  },
+  {
+    title: "PostgreSQL",
+    icon: SiPostgresql,
+    color: "text-sky-600",
+  },
+  {
+    title: "TailwindCSS",
+    icon: SiTailwindcss,
+    color: "text-cyan-500",
+  },
+];
+
+
+
+export {navLinks , skills}
 
