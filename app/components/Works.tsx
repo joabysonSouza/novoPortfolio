d8942651954281680001a35a90360d02b9a77329
Joabyson <joabyson916@gmail.com>
2026-01-14 22:23:33 -0300

    style:"aletrando a quantidade de colunas dos cards"

--- app/components/Works.tsx
+++ app/components/Works.tsx
@@ -30,7 +30,7 @@ export default function Works() {
       </p>
 
       <motion.div
-        className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4"
+        className="grid grid-cols-1 md:grid-cols-3 gap-3 px-4"
         variants={fadeIn}
         initial="hidden"
         whileInView="show"
