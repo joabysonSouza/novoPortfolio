a2996ef6ab9ff920d65db0e1efdf23be05a27b1d
Joabyson <joabyson916@gmail.com>
2026-02-11 21:35:43 -0300

    feat: "Importando navBar e adicionando os elementos de scroll "

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 
 "use client"
-import About from "./components/About";
+import Stacks from "./components/Stacks";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
 import dynamic from "next/dynamic";
 import LoadingSpinner from "./components/Spinner";
+import NavBar from "./components/NavBar";
+import {Element, Link} from "react-scroll"
 
-
+ 
 
 const SceneDynamic = dynamic(() => import("./components/Computer"), {
   ssr: false,
@@ -18,26 +20,34 @@ const SceneDynamic = dynamic(() => import("./components/Computer"), {
 const Home = () => {
 
 
+
+
   
   return (
     <>
+    <Element name="Inicio"> 
+       <NavBar />
+         </Element>
       <div className="w-96 m-6 text-center h-auto flex flex-col  md:w-full md:flex-row md:items-center md:h-screen  ">
+        
         <div>
           <h1 className=" font-anton mt-40  text-white text-3xl md:text-7xl">
             Olá eu sou <span className="text-red-600">Joabyson</span>
           </h1>
+          
 
           <p className="">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
             reprehenderit repudiandae
           </p>
         </div>
+      
 
         <SceneDynamic/>
       </div>
       
-        <About/>
-        <Works /> 
+        <Stacks />
+        <Works  /> 
         <Contact/>
      
     </>
