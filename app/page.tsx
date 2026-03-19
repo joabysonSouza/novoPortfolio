e23f4c5a876da06265c4827f3c28d6ff5d4eb348
Joabyson <joabyson916@gmail.com>
2026-03-18 22:25:55 -0300

    style: "alterando a cor de fundo "

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,16 @@ const Home = () => {
       <Element name="Inicio">
         <NavBar />
       </Element>
-      <div className="w-96 m-6 text-center h-auto flex flex-col  md:w-full md:flex-row md:items-center md:h-screen  ">
+      <div className="w-96 m-6 text-center h-auto  bg-black-transparent flex flex-col  md:w-full md:flex-row md:items-center md:h-screen  ">
         <div>
           <h1 className=" font-anton mt-40  text-white text-3xl md:text-7xl">
             Olá eu sou <span className="text-red-600">Joabyson</span>
           </h1>
 
-          <p className="p-4 mx-2 text-2xl">
+          <p className="flex justify-center p-4 m-2 text-2xl">
             Analista e Desenvolvedor de Sistemas FullStack
           </p>
-          <button> Curriculo </button>
+          
         </div>
 
         <SceneDynamic />
